265aa259479b807db1e03becc2c7d1e30a51a582
vinurs <haiyuan.vinurs@gmail.com>
2018-05-25 00:47:10 +0800

    :bookmark: release 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aliyun-dysms-clj "0.1.0-SNAPSHOT"
+(defproject aliyun-dysms-clj "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
