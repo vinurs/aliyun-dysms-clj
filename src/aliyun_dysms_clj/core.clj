18b600d40d7bb44b6e5b13a2224493bab9ae427c
vinurs <haiyuan.vinurs@gmail.com>
2018-05-24 23:00:14 +0800

    :pencil: add dependencies

--- src/aliyun_dysms_clj/core.clj
+++ src/aliyun_dysms_clj/core.clj
@@ -1,6 +1,11 @@
-(ns aliyun-dysms-clj.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(ns aliyun-dysms-clj.core
+  (:import (com.aliyuncs DefaultAcsClient IAcsClient)
+           (com.aliyuncs.dysmsapi.model.v20170525 QuerySendDetailsRequest
+                                                  QuerySendDetailsResponse
+                                                  SendSmsRequest
+                                                  SendSmsResponse)
+           (com.aliyuncs.dysmsapi.transform.v20170525.SendSmsResponseUnmarshaller)
+           (com.aliyuncs.exceptions ClientException)
+           (com.aliyuncs.http MethodType FormatType HttpResponse)
+           (com.aliyuncs.profile DefaultProfile IClientProfile)
+           (java.util Calendar GregorianCalendar)))
