dc0793abcd0f9602be5db97feea8ea9aef486742
vinurs <haiyuan.vinurs@gmail.com>
2018-05-25 08:21:17 +0800

    :pencil: 修改项目相关信息，发布clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject aliyun-dysms-clj "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject vinurs/aliyun-dysms-clj "0.1.0"
+  :description "阿里云发送短信接口clojure封装"
+  :url "https://github.com/vinurs/aliyun-dysms-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
