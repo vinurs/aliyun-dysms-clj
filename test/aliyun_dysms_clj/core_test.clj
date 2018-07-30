1d863d83bd6f59eeb2773c1263158a2b07921a49
vinurs <haiyuan.vinurs@gmail.com>
2018-07-30 09:28:52 +0800

    :sparkles: 增加设置阿里云key的测试用例

--- test/aliyun_dysms_clj/core_test.clj
+++ test/aliyun_dysms_clj/core_test.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [aliyun-dysms-clj.core :refer :all]))
 
+(deftest setconfig-test
+  (testing "测试设置阿里云key是否成功"
+    (let [config (aliyun-dysms-setconfig!
+                  {:access-key-id "123456"
+                   :access-key-secret "012345"})]
+      (is (and (= (:access-key-id config) "123456")
+               (= (:access-key-secret config) "012345"))))))
+
+
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
