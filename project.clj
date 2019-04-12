3179395b944924654878689e9b7574cae3818beb
vinurs <haiyuan.vinurs@gmail.com>
2019-04-12 23:07:52 +0800

    update dependencies version

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject vinurs/aliyun-dysms-clj "0.1.4"
+(defproject vinurs/aliyun-dysms-clj "0.1.5"
   :description "阿里云发送短信接口clojure封装"
   :url "https://github.com/vinurs/aliyun-dysms-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  ;; 阿里云短信服务
-                 [com.aliyun/aliyun-java-sdk-core "4.3.7"]
+                 [com.aliyun/aliyun-java-sdk-core "4.4.0"]
                  [cheshire "5.8.1"]])
